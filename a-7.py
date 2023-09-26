6dc862e4974844de96b9978cefabbb0b0161944e
minjyun3011 <grand3canyon10@gmail.com>
2023-09-26 10:00:31 +0900

    課題A完了！

---
+++ a-7.py
@@ -0,0 +1,6 @@
+even_numbers = [2, 4, 6, 8]
+
+for i in range(len(even_numbers)):
+    even_numbers[i] *= 2
+
+print(even_numbers)
---
+++ a-9.py
@@ -0,0 +1,9 @@
+bob_info = [["first_name", "Bob"],
+            ["family_name", "Dylan"],
+            ["age", 79]]
+
+bob_dict = dict(bob_info)
+
+print(bob_dict["first_name"]) # "Bob"
+print(bob_dict["family_name"]) # "Dylan"
+print(bob_dict["age"]) # 79
